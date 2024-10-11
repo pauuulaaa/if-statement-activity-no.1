ad4e58f6a4b2f12f0924766c23121556d03c3098
pauuulaaa <heleinapaula@gmail.com>
2024-10-11 11:28:23 +0800

    your commit message

---
+++ ageCheck.js
@@ -0,0 +1,11 @@
+function ageCheck() {
+    let age = prompt("Enter your Age.");
+        if (age <=13) {
+            alert("You are a child.");
+        } else if (age >=13 && age <=19) {
+            alert("You are a teenager.");
+        } else {
+            alert("You are an adult.");
+        }
+}
+
